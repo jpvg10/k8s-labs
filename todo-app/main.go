b984133636b910acb13437d280f6c4f7933be387
jpvg10 <juan.valencia10a@gmail.com>
2025-07-11 11:19:13 +0300

    hello world html

--- todo-app/main.go
+++ todo-app/main.go
@@ -10,11 +10,10 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLFiles("index.html")
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	port := os.Getenv("PORT")
