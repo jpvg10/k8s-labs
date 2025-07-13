f612870364be9241e953ed7061f713d2a12a1272
jpvg10 <juan.valencia10a@gmail.com>
2025-07-13 12:44:46 +0300

    log output ingress

--- log-output/index.js
+++ log-output/index.js
@@ -1,7 +1,17 @@
+const http = require('http')
+
 const num = Math.floor(Math.random() * 1000000)
 const str = `string-${num}`
 
-setInterval(() => {
-    const date = new Date().toJSON();
-    console.log(`${date}: ${str}`)
-}, 5000)
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    const date = new Date().toJSON()
+    res.end(`${date}: ${str}\n`)
+})
+
+const port = process.env.PORT || 3000
+const host = '0.0.0.0'
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`)
+})
